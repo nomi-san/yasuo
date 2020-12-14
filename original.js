05c966395efcea9dc34f65098713621f5ccaa170
Nomi <wuuyi123@gmail.com>
2020-12-14 17:35:19 +0700

    Turn all into ES6

--- original.js
+++ original.js
@@ -1,44 +1,67 @@
 var start = (championIds = [157]) => {
-    // Simple request
-    var request = async (method, url, body = undefined) => {
+    
+    // Simple request with fetch API
+    const request = async (url, method = 'GET', body = undefined) => {
         const data = await fetch(url, {
             method: method,
-            body: body,
-            headers: {'Content-type': 'application/json; charset=UTF-8'}
-        }).then(res => res.text())
-          .then(txt => JSON.parse(txt.length ? txt : '{}'));
-        return data;
-    };
+            body: JSON.stringify(body),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+        }).then(res => res.text()).then(txt =>
+            JSON.parse(txt || '{}')
+        )
+        return data
+    }
+    
     // Check if match found is shown
-    var isInProgress = async () => (await request(
-        'GET', '/lol-matchmaking/v1/ready-check')).state === 'InProgress';
+    var isInProgress = async () => (
+        (await request('/lol-matchmaking/v1/ready-check'))
+            .state === 'InProgress'
+    )
+    
     // Accept match
-    var acceptMatch = async () => await request(
-        'POST', '/lol-matchmaking/v1/ready-check/accept');
+    var acceptMatch = async () => (
+        await request('/lol-matchmaking/v1/ready-check/accept', 'POST')
+    )
+    
     // Get your action ID
     var getActionId = async () => {
-        var {localPlayerCellId, actions} =
-            await request('GET', '/lol-champ-select/v1/session');
-        if (!actions) return -1;
-        return actions[0].filter(v =>
-            v.actorCellId === localPlayerCellId)[0].id;
-    };
+        const { localPlayerCellId, actions } = await request('/lol-champ-select/v1/session')
+        if (!actions) return -1
+        return actions[0] // Index 0 is our team
+            .find(a => a.actorCellId === localPlayerCellId) // Just find current player
+            .id // We found action ID
+    }
+    
     // Pick a champion
-    var pick = async (id, championId) => Object.keys(await request(
-        'PATCH', `/lol-champ-select/v1/session/actions/${id}`,
-        JSON.stringify({championId}))).length === 0;
-    // Lock selection
-    var lock = async (id) => await request(
-        'POST', `/lol-champ-select/v1/session/actions/${id}/complete`);
+    var pick = async (id, championId) => (
+        Object.keys(await request(
+            `/lol-champ-select/v1/session/actions/${id}`,
+            'PATCH', { championId }
+        )).length === 0 // Succeed
+    )
+    
+    // Lock the selection
+    var lock = async (id) => (
+        await request(`/lol-champ-select/v1/session/actions/${id}/complete`, 'POST')
+    )
+    
     // Start auto accept match found and pick-lock
     var id, inv = setInterval(async () => {
+        // Check if is in accepting match found
         if (await isInProgress()) {
-            await acceptMatch();
+            // Accept match
+            await acceptMatch()
         } else if ((id = await getActionId()) > -1) {
-            for (var i=0; i<championIds.length; i++)
-                if (await pick(id, championIds[i])) break;
-            await lock(id);
-            clearInterval(inv);
+            // Pick each champs
+            for (var i = 0; i < championIds.length; ++i) {
+                if (await pick(id, championIds[i])) {
+                    break // Break on picked done
+                }
+            }
+            // Lock
+            await lock(id)
+            // Stop this callback
+            clearInterval(inv)
         }
-    }, 250);
-};
+    }, 250) // Default timeout is 250ms
+}
